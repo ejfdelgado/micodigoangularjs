a8e129520da6b8000e77dd184662a348370ef255
ejfdelgado <edelgado@heinsohn.com.co>
2018-09-10 17:59:19 -0500

    Se ajusta drag drop

--- prueba.js
+++ prueba.js
@@ -35,9 +35,27 @@ var prueba = {
 	]
 };
 
+//pc-3255-hngx1
+
 var respuesta = moduloTransformacion.modo('simple').to(prueba);
 var respuesta2 = moduloTransformacion.modo('simple').from(respuesta);
 
 console.log(JSON.stringify(respuesta, null, 4));
 console.log(JSON.stringify(respuesta2, null, 4));
 console.log(JSON.stringify(prueba, null, 4));
+
+$(document).ready(function() {
+	
+	var droppable = new Draggable.Droppable(document.querySelectorAll('.padre'), {
+	  draggable: '.arrastrame',
+	  droppable: '.destino'
+	});
+	
+	
+	droppable.on('droppable:dropped', () => console.log('droppable:dropped'));
+	droppable.on('droppable:returned', () => console.log('droppable:returned'));
+	droppable.on("droppable:over", function() {
+	  $(".destino").removeClass("draggable-droppable--occupied");
+	});
+	
+});
